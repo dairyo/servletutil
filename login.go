abec1fed8603f63d3095059d39c9357501b2b1dd
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-07-31 15:37:48 +0900

    Fix Login.

--- login.go
+++ login.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
-	"strings"
 )
 
-func Login(endpoint, username, password string) (sid string, err error) {
+type Session struct {
+	Id  string
+	Key string
+}
+
+func Login(endpoint, username, password string) (session *Session, err error) {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	client := http.Client{Jar: jar}
 
@@ -19,25 +24,26 @@ func Login(endpoint, username, password string) (sid string, err error) {
 	values.Set("j_username", username)
 	values.Set("j_password", password)
 
-	req, err := http.NewRequest(
-		"POST",
-		endpoint,
-		strings.NewReader(values.Encode()))
+	resp, err := client.PostForm(endpoint, values)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-
-	resp, err := client.Do(req)
 	defer resp.Body.Close()
-	if err != nil {
-		return "", err
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, fmt.Errorf("Status Code %d", resp.StatusCode)
 	}
 
 	set_cookie_url, err := url.Parse(endpoint)
-	cookies := jar.Cookies(set_cookie_url)
-	sid = cookies[0].Value
+	if err != nil {
+		return nil, err
+	}
+	for _, cookie := range jar.Cookies(set_cookie_url) {
+		if cookie.Name == "JSESSIONID" {
+			return &Session{Id: cookie.Value, Key: cookie.Name}, nil
+		}
+	}
 
-	return sid, nil
+	return nil, errors.New("No cookies")
 }
 
 func main() {
