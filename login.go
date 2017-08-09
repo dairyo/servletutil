f1e6fc5247855daea8519c02410f7d8e865ed6f6
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-08-09 16:18:34 +0900

    Implement CreateUser.

--- login.go
+++ login.go
@@ -6,12 +6,19 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strings"
 )
 
 // Session contains session id and key. They are cookie values.
 type Session struct {
 	ID  string
 	Key string
+}
+
+// Drools-wb user infomation.
+type User struct {
+	Username string
+	Password string
 }
 
 // Login to drools-wb endpoint with username and password.
@@ -69,6 +76,7 @@ func Login(endpoint, username, password string) (session *Session, err error) {
 	return nil, errors.New("session not found")
 }
 
+// Logout drools-wb.
 func Logout(endpoint string, session *Session) (err error) {
 	u, err := url.Parse(endpoint)
 	if err != nil {
@@ -102,5 +110,77 @@ func Logout(endpoint string, session *Session) (err error) {
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unexpected status code %d", resp.StatusCode)
 	}
+	return nil
+}
+
+// Create drools-wb user.
+func CreateUser(endpoint string, newUser, restUser User) (err error) {
+	// Create client.
+	client := http.Client{}
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return err
+	}
+	// Create user.
+	u2, err := u.Parse("drools-wb/rest/user")
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest(http.MethodPost, u2.String(),
+		strings.NewReader(newUser.Username))
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(restUser.Username, restUser.Password)
+	req.Header.Set("content-type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+	// Add a role.
+	u3, err := u.Parse("drools-wb/rest/user/roles/" + newUser.Username)
+	if err != nil {
+		return err
+	}
+	req, err = http.NewRequest(http.MethodPut, u3.String(),
+		strings.NewReader("[\"analyst\"]"))
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(restUser.Username, restUser.Password)
+	req.Header.Set("content-type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+	// Set password
+	u4, err := u.Parse("drools-wb/rest/user/password/" +
+		newUser.Username + "?password=" + newUser.Password)
+	if err != nil {
+		return err
+	}
+	req, err = http.NewRequest(http.MethodPut, u4.String(),
+		strings.NewReader(""))
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(restUser.Username, restUser.Password)
+	resp, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+
 	return nil
 }
