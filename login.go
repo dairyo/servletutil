b7456042738288f2194abdf01c143993da4e0096
奈良信介 <narashinsuke@narashinsukenoMacBook-Pro.local>
2017-08-21 16:10:15 +0900

    Implement proxy for drools workbench.

--- login.go
+++ login.go
@@ -3,6 +3,9 @@ package servletutil
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -183,4 +186,44 @@ func CreateUser(endpoint string, newUser, restUser User) (err error) {
 	}
 
 	return nil
+}
+
+func ProxyDroosWB(proxyURL, targetURL string) (proxy *http.Server) {
+	return &http.Server{
+		Addr: proxyURL,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			d, err := net.Dial("tcp", targetURL)
+			if err != nil {
+				http.Error(w, "Error contacting backend server.", 500)
+				log.Printf("Error dialing websocket backend %s: %v", targetURL, err)
+				return
+			}
+			hj, ok := w.(http.Hijacker)
+			if !ok {
+				http.Error(w, "Not a hijacker?", 500)
+				return
+			}
+			nc, _, err := hj.Hijack()
+			if err != nil {
+				log.Printf("Hijack error: %v", err)
+				return
+			}
+			defer nc.Close()
+			defer d.Close()
+
+			err = r.Write(d)
+			if err != nil {
+				log.Printf("Error copying request to target: %v", err)
+				return
+			}
+
+			errc := make(chan error, 2)
+			cp := func(dst io.Writer, src io.Reader) {
+				_, err := io.Copy(dst, src)
+				errc <- err
+			}
+			go cp(d, nc)
+			go cp(nc, d)
+			<-errc
+		})}
 }
